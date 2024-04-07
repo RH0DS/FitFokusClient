904717a1dcad6337fdc079d3ae920c0fd5032ca3
Robin <rhods15@hotmail.com>
2024-04-08 00:14:54 +0200

    add the user profile from google login to profilePage

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,12 +1,43 @@
 
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
-const ProfilePage = () => 
-{
+
+const ProfilePage = () => {
+    
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+    return <div>Loading ...</div>;
+    }
+
     return (
-    
-    <h1>ProfilePage kommer visa dina sparade uppgifter </h1>
-    
-    
+        isAuthenticated && (
+            <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}1</h2>
+            <p>{user.given_name} Given Name</p>
+            <p>{user.family_name} Family name</p>
+            <p>{user.email} 2 email</p>
+            <p>{user.address} 3 address</p>
+            <p>{user.birthdate} 4 birthdate</p>
+            <p>{user.gender} 5 gender</p>
+            <p>{user.locale} 6 locale</p>
+            <p>{user.nickname} 7 nickname</p>
+            <p>{user.phone_number} 8 phone_number</p>
+            <p>{user.preferred_username} 9 preferred_username</p>
+            <p>{user.profile} 10 profile</p>
+            <p>{user.sub} 11 sub</p>
+            <p>{user.updated_at} 12 updated_at</p>
+            <p>{user.zoneinfo} 13 zoneinfo</p>
+            <p>{user.email_verified.toString()} 14 email_verified</p>
+            <p>{user.website} 15 website</p>
+            <p>{user.phone_number_verified} 15 phone_number_verified</p>
+            
+            
+            
+            </div>
+        )
     );
 };
 export default ProfilePage;
