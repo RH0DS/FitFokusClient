075636446a835ff9d13393348d9b89dc2e4e0450
Robin <rhods15@hotmail.com>
2024-04-13 19:36:42 +0200

    Github actions tests

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,8 +1,10 @@
 const Dashboard = () => {
-    return ( 
+    return ( <>
     <h1>Dashboard / home page / what to call this page? </h1>
 
-    
+    <h1>Hey from Github Actions</h1>
+
+    </>
     );
   };
   
