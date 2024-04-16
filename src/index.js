a709aadd60d053619013686c50a36cf9765e415a
Robin <rhods15@hotmail.com>
2024-04-16 12:38:32 +0200

    build secrets instead

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const FitFokusClientId = process.env.REACT_APP_AUTH_CLIENTID
 const FitFokusDomain = process.env.REACT_APP_AUTH_DOMAIN
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
